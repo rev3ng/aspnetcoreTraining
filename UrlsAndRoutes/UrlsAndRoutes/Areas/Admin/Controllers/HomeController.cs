3519a1bfd49607ee2993e5336f631f01a302fb45
rev3ng <konsle17@wp.pl>
2018-10-15 16:53:07 +0200

    UrlsAndRoutes

---
+++ UrlsAndRoutes/UrlsAndRoutes/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Areas.Admin.Models;
+
+namespace UrlsAndRoutes.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    public class HomeController : Controller
+	{
+		private Person[] data = new Person[]
+		{
+			new Person {City = "Londyn", Name = "Alicja"},
+			new Person {City = "Nowy Jork", Name = "Bartek"},
+			new Person {City = "Warszawa", Name = "Janek"}
+		};
+
+        public IActionResult Index()
+        {
+            return View(data);
+        }
+    }
+}
---
+++ UrlsAndRoutes/UrlsAndRoutes/Infrastructure/WeekDayConstraint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+	public class WeekDayConstraint : IRouteConstraint
+	{
+		private static string[] days = new[]
+		{
+			"pon", "wt", "sr",
+			"czw", "pt", "sob", "nie"
+		};
+
+		public bool Match(HttpContext httpContext, IRouter route, 
+			string routeKey, RouteValueDictionary values,
+			RouteDirection routeDirection)
+		{
+			return days.Contains(values[routeKey]?.ToString().ToLowerInvariant());
+		}
+	}
+}
