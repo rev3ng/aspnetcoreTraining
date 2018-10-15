15a4a4c6bef4bcd5f553f99de4edef299bd5f2ef
rev3ng <konsle17@wp.pl>
2018-10-15 16:51:43 +0200

    DependencyInjection

---
+++ DependencyInjection/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+	    private IRepository repository;
+	    private ProductTotalizer totalizer;
+
+	    public HomeController(IRepository repo, ProductTotalizer total)
+	    {
+		    repository = repo;
+		    totalizer = total;
+	    }
+
+	    public ViewResult Index()
+	    {
+		    ViewBag.HomeController = repository.ToString();
+		    ViewBag.Totalizer = totalizer.Repository.ToString();
+		    ViewBag.Total = totalizer.Total;
+		    return View(repository.Products);
+	    }
+    }
+}
---
+++ DependencyInjection/DependencyInjection/Models/MemoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+	public class MemoryRepository : IRepository
+	{
+		private IModelStorage storage;
+
+		private string guid = Guid.NewGuid().ToString();
+		//private Dictionary<string, Product> products;
+
+		public MemoryRepository(IModelStorage modelStore)
+		{
+			storage = modelStore;
+			new List<Product>
+			{
+				new Product {Name = "Kajak", Price = 275M},
+				new Product {Name = "Kamizelka ratunkowa", Price = 48.95M},
+				new Product {Name = "Piłka nożna", Price = 19.50M},
+			}.ForEach(p => AddProduct(p));
+		}
+
+		public IEnumerable<Product> Products => storage.Items;
+		public Product this[string name] => storage[name];
+		public void AddProduct(Product product) => storage[product.Name] = product;
+		public void DeleteProduct(Product product) => storage.RemoveItem(product.Name);
+
+		public override string ToString()
+		{
+			return guid;
+		}
+	}
+}
