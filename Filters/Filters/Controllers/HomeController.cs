fbff450e32b8ed6485811da54055f62fbd0d0e3d
rev3ng <konsle17@wp.pl>
2018-10-15 16:52:47 +0200

    Filters

---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+	[Message("To jest filtr o zasięgu kontrolera.")]
+    public class HomeController : Controller
+    {
+		[Message("To jest pierwszy filtr o zasięgu akcji.")]
+		[Message("To jest drugi filtr o zasięgu akcji.")]
+	    public ViewResult Index()
+	    {
+		    return View("Message",
+			    "To jest metoda akcji Index() kontrolera HomeController");
+	    }
+
+    }
+}
