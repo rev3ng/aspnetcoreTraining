eb716d0d04c6ccfb90211f89d17355dc1c766ad9
rev3ng <konsle17@wp.pl>
2018-10-16 15:11:48 +0200

    Using ViewComponents finish

---
+++ UsingViewComponents/UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+	public class CitySummary : ViewComponent
+	{
+		private ICityRepository repository;
+
+		public CitySummary(ICityRepository repo)
+		{
+			repository = repo;
+		}
+
+		/*public string Invoke()
+		{
+			return $"{repository.Cities.Count()} miasta, "
+			       + $"{repository.Cities.Sum(c => c.Population)} osób";
+		}
+		*/
+
+		/*
+		public IViewComponentResult Invoke()
+		{
+			return View(new CityViewModel
+			{
+				Cities = repository.Cities.Count(),
+				Population = repository.Cities.Sum(c => c.Population)
+			});
+		}
+		*/
+
+		/*
+		public IViewComponentResult Invoke()
+		{
+			return Content("To jest <h3><i>ciąg tekstowy</i></h3>.");
+		}
+		*/
+
+		/*
+		public IViewComponentResult Invoke()
+		{
+			return new HtmlContentViewComponentResult(
+				new HtmlString("To jest <h3><i>ciąg tekstowy</i></h3>."));
+		}
+		*/
+
+		/*
+		public IViewComponentResult Invoke()
+		{
+			string target = RouteData.Values["id"] as string;
+			var cities = repository.Cities
+				.Where(city => target == null ||
+				               string.Compare(city.Country, target, true) == 0);
+			return View(new CityViewModel
+			{
+				Cities = cities.Count(),
+				Population = cities.Sum(c => c.Population)
+			});
+		}
+		*/
+
+		public IViewComponentResult Invoke(bool showList)
+		{
+			if (showList)
+			{
+				return View("CityList", repository.Cities);
+			}
+			else
+			{
+				return View(new CityViewModel
+				{
+					Cities = repository.Cities.Count(),
+					Population = repository.Cities.Sum(c => c.Population)
+				});
+			}
+		}
+	}
+}
---
+++ UsingViewComponents/UsingViewComponents/Components/PageSize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace UsingViewComponents.Components
+{
+	public class PageSize : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			HttpClient client = new HttpClient();
+			HttpResponseMessage response = await client.GetAsync("https://www.apress.com/gp");
+			return View(response.Content.Headers.ContentLength);
+		}
+	}
+}
