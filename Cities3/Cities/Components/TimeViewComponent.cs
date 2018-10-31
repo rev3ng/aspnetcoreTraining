41fb4e916c1f343f15ea983945bb07d1537e909b
rev3ng <konsle17@wp.pl>
2018-10-31 14:21:17 +0100

    Model binding

---
+++ Cities3/Cities/Components/TimeViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cities3.Components
+{
+	public class TimeViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View(DateTime.Now);
+		}
+	}
+}
