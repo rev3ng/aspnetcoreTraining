0b07fa5461f03c863bd4c1ffd5ea2cfbad631dbf
rev3ng <konsle17@wp.pl>
2018-11-05 12:20:56 +0100

    .

---
+++ Users/Users/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Users.Controllers
+{
+    public class HomeController : Controller
+    {
+	    public ViewResult Index() =>
+		    View(new Dictionary<string, object>
+		    {["Miejsce zarezerwowane"] = "Miejsce zarezerwowane" });
+    }
+}
